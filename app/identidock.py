6080210047d630d9a9dc32d21fd6793486e9aa44
Musa <tamzid101@gmail.com>
2017-12-07 00:39:15 +0600

    ok now image is changing according to the name :)

--- app/identidock.py
+++ app/identidock.py
@@ -1,24 +1,45 @@
 from flask import Flask
 from flask import Response
+from flask import request
+
 import requests
+import hashlib
 
 
 
 app = Flask(__name__)
 default_name = "Musa"
+salt = "UNIQUE_SALT"
+
+
+def get_name():
+
+
+    if request.method == "POST":
+        name = request.form["name"]
+    else:
+        name = default_name
+
+    return name
+
+
 
 
 @app.route("/" , methods = ["GET" , "POST"])
 def mainpage():
 
-    name = default_name
+    name = get_name()
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
     header = "<html><head><title>Identidock</title></head><body>"
     body = '''<form method = "POST">
-                  Hello <input type = "text" name = "name" value = "{}" >
+                  Hello <input type = "text" name = "name" value = "{0}" >
                   <input type = "submit" value = "submit">
             </form>
-            <p>You look like : <img src = "/monster/monster.png"/></p>
-    '''.format(name)
+            <p>You look like : <img src = "/monster/{1}"/></p>
+    '''.format(name , name_hash)
     footer = "</body></html>"
 
 
@@ -27,6 +48,8 @@ def mainpage():
 @app.route("/monster/<name>")
 def get_identicon(name):
 
+
+    # requests a web app written in node to return image.
     r = requests.get("http://dnmonster:8080/monster/" + name + "?size=80")
     image = r.content
 
